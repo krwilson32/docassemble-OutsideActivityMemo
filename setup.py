a584df256efda5db28bce0bdbd0125566b3231ea
Anonymous User <70125200+krwilson32@users.noreply.github.com>
2020-12-14 14:21:18 -0500

    second attempt at saving

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.OutsideActivityMemo',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.OutsideActivityMemo\n\nA docassemble extension.\n\n## Author\n\nkwilson5@su.suffolk.edu\n\n',
+      long_description='# docassemble.OutsideActivityMemo\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nkwilson5@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Kisha',
       author_email='kwilson5@su.suffolk.edu',
